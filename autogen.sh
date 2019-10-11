2b9766d31e1da9db0b58fb761df2b9a02b71d3df
Hexiang Qi <qihexiang@outlook.com>
2019-10-11 23:14:00 +0800

    修正了SSH密钥生成中的逻辑错误

--- autogen.sh
+++ autogen.sh
@@ -9,18 +9,20 @@ if [ ! $INTERNAL_TIME ]; then
 fi
 
 ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -N $REPO_URL 
-if [ ! -d .ssh ] || [ ! -f .id_rsa ]; then
+if [ ! -d .ssh ] || [ ! -f .ssh/id_rsa ]; then
     mkdir .ssh 2>/dev/null
     ssh-keygen -P "" -f ~/.ssh/id_rsa -q
     find -type d -exec chmod 700 {} \;
     find -type d -exec chmod 600 {} \;
+    # 提醒用户将密钥添加到远程服务器，并等待60s
+    echo "Please install this key to your git account: "
+    cat ~/.ssh/id_rsa.pub
+    echo "Program will wait 60s before run the first process, please install the key to your git repo."
+    sleep 60s
+else
+    echo "SSH Key is: "
+    cat ~/.ssh/id_rsa.pub
 fi
-
-# 提醒用户将密钥添加到远程服务器，并等待60s
-echo "Please install this key to your git account: "
-cat ~/.ssh/id_rsa.pub
-echo "Program will wait 60s before run the first process, please install the key to your target server first, and then to your git repo."
-sleep 60s
 
 # 若不存在目录，则创建并进行第一次拉取和安装
 if [ ! -d hexo ]; then
